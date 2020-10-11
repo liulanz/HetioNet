f1896acacaf3a3cc6d27d9c5ab196c37c748a7f3
liulanz <zhengliulan@gmail.com>
2020-10-11 15:52:08 -0400

    added main.py

---
+++ Hetio-Net/main.py
@@ -0,0 +1,40 @@
+
+
+
+MESSGAE = '''
+CHOICE A) Given a disease, what is its name, what are drug names that can treat or
+palliate this disease, what are gene names that cause this disease, and where
+this disease occurs?
+
+CHOICE B) Supposed that a drug can treat a disease if the drug or its similar drugs
+up-regulate/down-regulate a gene, but the location down-regulates/up-regulates
+the gene in an opposite direction where the disease occurs. Find all drugs
+that can treat new diseases (i.e. the missing edges between drug and disease).
+
+ENTER "A" for CHOICE A
+ENTER "B" for CHOICE B
+'''
+
+
+
+
+def main():
+    
+
+    print(MESSGAE)
+
+    choice = input("Please enter your choice: ")
+    while(choice != "A" || choice != "B"){
+        choice = input("Invalid Choice. Please re-enter << ")
+    }
+
+
+    if choice == 'A':
+        query = input("Please enter a disease name: ")
+  
+
+    
+
+
+if __name__ == "__main__":
+    main()
---
+++ Sample_codes/py2neo.py
@@ -0,0 +1,6 @@
+from py2neo import Graph
+graph = Graph()
+from py2neo import Node, Relationship
+
+#create a node
+nicole = Node("Person")
