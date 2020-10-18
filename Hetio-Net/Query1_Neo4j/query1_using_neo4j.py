24c53eb6622119af4f763d2fa677bbb89b836a6b
liulanz <zhengliulan@gmail.com>
2020-10-18 18:59:41 -0400

    tested with multiple nodes, added graph using networkx, updated tsv

--- Hetio-Net/Query1_Neo4j/query1_using_neo4j.py
+++ Hetio-Net/Query1_Neo4j/query1_using_neo4j.py
@@ -3,8 +3,9 @@ from py2neo.data import Node, Relationship
 from py2neo.ogm import *
 import csv
 from tkinter import *
-import matplotlib
-matplotlib.use('Agg')
+import networkx as nx
+import matplotlib.pyplot as plt
+G = nx.DiGraph()
 
 MESSAGAE = '''
 ====================================================================================================
@@ -22,7 +23,9 @@ MESSAGAE = '''
 ====================================================================================================
 '''
 
-
+######################################################################
+####### Breaking down tsv file and store into Neo4j Database #########
+######################################################################
 
 def getRelationship(letter):
     relationship = ""
@@ -88,6 +91,11 @@ def readEdges():
 
     edges_tsv_file.close()
 
+
+######################################################################
+############################ Query 1##################################
+######################################################################
+
 # Given a disease id, 
 # what is its name, 
 # what are drug names that can treat or palliate this disease, 
@@ -96,7 +104,7 @@ def readEdges():
 # Obtain and output this information in a single query.
 def queryDisease(diseaseID):
 	global window
-	query1_message = Text(window, width=100, height=10, wrap=WORD, background="white")
+	query1_message = Text(window, width=100, height=2, wrap=WORD, background="white")
 	query1_message.grid(row=16, column=0, columnspan=2, sticky=W)
 	query = f"""
         MATCH (c:Compound)-[:treats|palliates]->(d:Disease {{id: "{diseaseID}"}})
@@ -109,25 +117,79 @@ def queryDisease(diseaseID):
         #print("No record found")
 		query1_message.insert(END, "No record found. Please re-enter.")
 	else:
-		compound_name=""
-		gene_name=""
-		anatomy_name=""
+		compound_names=[]
+		gene_names=[]
+		anatomy_names=[]
 		for result in results:
-			compound_name = result['c.name']
-			gene_name = result['g.name']
-			anatomy_name = result['a.name']
-		query1_result = f"""
-			Disease ID: {diseaseID}
-    		Disease Name: {result['d.name']}
-    		Drugs that treat/palliates this disease: {compound_name}
-    		Genes that cause cause this disease: {gene_name}
-    		This disease occurs at: {anatomy_name}
-		"""
-	        # print(f"\t{result['d.name']}")
-	        # print(f"\t{result['c.name']}")
-	        # print(f"\t{result['g.name']}")
-	        # print(f"\t{result['a.name']}")
-		query1_message.insert(END, query1_result)
+			# avoid adding "None" node to the graph
+			if(str(result['c.name'])!="None"):
+				compound_names.append(result['c.name'])
+			if(str(result['g.name'])!="None"):
+				gene_names.append(result['g.name'])
+			if(str(result['a.name'])!="None"):
+				anatomy_names.append(result['a.name'])
+			
+
+		
+		disease = str(result['d.name'] +"\n"+diseaseID )
+		######################################################################
+		#### Creating a graph as an output using networkx and matplotlib #####
+		######################################################################
+
+		# avoid adding "None" node to the graph
+		if(len(compound_names)!=0):
+			for compound_name in compound_names:
+				G.add_edge((str)(compound_name),disease)
+		if(len(gene_names)!=0):
+			for gene_name in gene_names:
+				G.add_edge((str)(gene_name), disease)
+				
+		if(len(anatomy_names)!=0):
+			for anatomy_name in anatomy_names:
+				G.add_edge(disease, (str)(anatomy_name))
+
+		pos = nx.spring_layout(G)
+
+		# drawing edges and nodes
+		nx.draw(G,pos,edge_color='red',width=1,linewidths=1, node_size=2000,node_color='#9999FF',alpha=0.8,arrowsize=20, arrows= True,labels={node:node for node in G.nodes()  })
+		
+
+		# adding edge label to each edge
+		if(len(compound_names)!=0):
+			for compound_name in compound_names:
+				nx.draw_networkx_edge_labels(G,pos,edge_labels={((str)(compound_name), disease):'treats/paliates'},font_color='red')
+		if(len(gene_names)!=0):
+			for gene_name in gene_names:
+				nx.draw_networkx_edge_labels(G,pos,edge_labels={((str)(gene_name),disease):'associates'},font_color='red')
+		if(len(anatomy_names)!=0):
+			for anatomy_name in anatomy_names:
+				nx.draw_networkx_edge_labels(G,pos,edge_labels={(disease,(str)(anatomy_name)):'localizes'},font_color='red')
+		
+		
+
+		
+		# if(compound_name!="None"):
+		# 	nx.draw_networkx_edge_labels(G,pos,edge_labels={(compound_name, result['d.name']+ "\n"+diseaseID):'treats/paliates'},font_color='red')
+		# if(gene_name!="None"):
+		# 	nx.draw_networkx_edge_labels(G,pos,edge_labels={(gene_name,result['d.name']+"\n"+diseaseID):'associates'},font_color='red')
+		# if(anatomy_name!="None"):
+		# 	nx.draw_networkx_edge_labels(G,pos,edge_labels={(result['d.name']+"\n"+diseaseID,anatomy_name):'localizes'},font_color='red')
+		plt.show()
+
+
+
+		# query1_result = f"""
+		# 	Disease ID: {diseaseID}
+  #   		Disease Name: {result['d.name']}
+  #   		Drugs that treat/palliates this disease: {compound_name}
+  #   		Genes that cause cause this disease: {gene_name}
+  #   		This disease occurs at: {anatomy_name}
+		# """
+	 #        # print(f"\t{result['d.name']}")
+	 #        # print(f"\t{result['c.name']}")
+	 #        # print(f"\t{result['g.name']}")
+	 #        # print(f"\t{result['a.name']}")
+		# query1_message.insert(END, query1_result)
   
 
 
@@ -166,6 +228,9 @@ def queryCompound(compound):
         for result in results:
             print(f"\t{result['c.name']}-{result['d.name']}")
 
+######################################################################
+##################### functions for GUI part #########################
+######################################################################
 def query1():
 	global textentry
 	disease_id = textentry.get()
@@ -183,7 +248,7 @@ def choiceClick():
 	
 	if choice == 'A':
 		choice_message.insert(END, "CHOICE A was entered.")
-		Label(window, text="Please enter a disease ID: ", bg="black", fg="white", font="none 12 bold") .grid(row=9,column=0, sticky=W)
+		Label(window, text="Please enter a disease ID: ", bg="#856ff8", fg="white", font="none 12 bold") .grid(row=9,column=0, sticky=W)
 		textentry = Entry(window, width=20, bg="white")
 		textentry.grid(row=10,column=0,sticky=W)
 		Button(window, text="SUBMIT", width=6, command=query1) .grid(row=12, column=0, sticky=W)		
@@ -199,13 +264,16 @@ def closeWindow():
 	window.destroy()
 	exit()
 
-#################################################################################################
+######################################################################
+##################### Creating GUI using tkinter######################
+######################################################################
+
 graph = Graph("bolt://localhost:7687", auth=("neo4j", "password"))
 graph.delete_all()
 
 window = Tk()
-window.title("Hetio-Net by Yi Heng & Liulan ")
-window.configure(background="black")
+window.title("Big Data Technology Project I by Yi Heng & Liulan ")
+window.configure(background="#856ff8")
 
 
 readNodes()
@@ -213,26 +281,26 @@ readEdges()
 
 
 #https://www.youtube.com/watch?v=_lSNIrR1nZU
-
-
-	#adding image
+#adding image
 #photo1 = PhtoImage(file=" ")
-Label(window, text=MESSAGAE, bg="black", fg="white", font="none 12 bold") .grid(row=0,column=0, sticky=W)
-
-
-# Enter choice 
-Label(window, text="Please enter your choice: ", bg="black", fg="white", font="none 12 bold") .grid(row=2,column=0, sticky=W)
+Label(window, text=MESSAGAE, bg="#856ff8",fg="white", font="none 12 bold") .grid(row=0,column=0, sticky=W)
+# Enter choice eiher 'A' or 'B'
+Label(window, text="Please enter your choice: ", bg="#856ff8", fg="white", font="none 12 bold") .grid(row=2,column=0, sticky=W)
 textentry = Entry(window, width=20,bg="white")
 textentry.grid(row=4,column=0,sticky=W)
-Button(window, text="SUBMIT CHOICE", width=12, command=choiceClick) .grid(row=6, column=0, sticky=W)
+Button(window, text="SUBMIT CHOICE", width=14, command=choiceClick) .grid(row=6, column=0, sticky=W)
 choice_message = Text(window, width=40, height=1, wrap=WORD, background="white")
 choice_message.grid(row=7, column=0, columnspan=2, sticky=W)
 
 
 # Exit button
-#Label(window, text="Click to exit: ", bg="black", fg="white", font="none 12 bold") .grid(row=40,column=0, sticky=E)
 Button(window, text="Exit", width=10, command=closeWindow) .grid(row=42, column=0, sticky=E)
 
+
+window.mainloop()
+
+
+############## Without GUI #############################
 
 # choice = input("Please enter your choice: ")
 # while(choice != "A" and choice != "B"):
@@ -247,12 +315,13 @@ Button(window, text="Exit", width=10, command=closeWindow) .grid(row=42, column=
 # if choice == 'B':
 #     query = input("Please enter a compound name: ")
 #     queryCompound(query)
-window.mainloop()
+
 
 # if __name__ == "__main__":
 #     main()
 
 
+############Liulan's running command reference###########
 # sudo systemctl status neo4j.service
 # sudo service neo4j start
 
